60281d5dad3df4bd76591d1b80769aa58319964f
Tyler Holland <tyler@debian.univm>
2016-07-28 16:10:15 +1200

    used indent to correct formatting

--- SuperQuiz_1/accelerometer.c
+++ SuperQuiz_1/accelerometer.c
@@ -1,25 +1,41 @@
+/*
+ * Super Quiz 1 ENCE260
+ * @author Tyler Holland
+ * Last Edited: 28/07/2016
+ */
+
 #include <stdio.h>
+
+/*
+ * calculates peak magnitudes of
+ *  acceleration
+ */
 
 int main(void)
 {
-	
-	//unsigned int t = 0;
-	int input = 0;
-	int peak = 0;
-	int previous = 0;
-	int i = 0;
-	printf("Number  Magnitude\n");
-	do {
-		scanf("%u", &input);
-		if(input >= peak) {
-			peak = input;
-		} else if (input != -1 && input < peak && previous == peak) {
-			i += 1;
-			printf("%4.u%11.u\n", i, peak);
-		} else {
-			peak = previous;
-		}
-		previous = input;
-	} while(input != -1);
-	return 0;
+
+    int input = 0;
+    int peak = 0;
+    int previous = 0;
+    int t = 0;
+
+    printf("Number  Magnitude\n");
+
+    do {
+        scanf("%u", &input);
+
+        if (input != -1 && input >= peak) {
+            peak = input;
+        } else if (input != -1 && input < peak && previous == peak) {
+            t += 1;
+            printf("%4.u%11.u\n", t, peak);
+        } else {
+            peak = previous;
+        }
+
+        previous = input;
+
+    } while (input != -1);
+
+    return 0;
 }
